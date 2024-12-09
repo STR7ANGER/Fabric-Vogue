19ae7fec2dee7dedfff6875d72af79d8b8e20b61
STR7ANGER <adityamaurya.2807@gmail.com>
2024-12-09 21:54:01 +0530

    component,context

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import { ShopContext } from '../context/ShopContext'
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import {Link} from 'react-router-dom'
 
-const ProductItem = ({}) => {
+const ProductItem = ({ id, name, price,image }) => {
+  const { currency } = useContext(ShopContext);
   return (
-    <div>ProductItem</div>
-  )
-}
+    <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
+      <div className="overflow-hidden">
+        <img className="hover:scale-110 transition ease-in-out" src={image[0]} alt=""/>
+      </div>
+      <p className="pt-3 pb-1 text-sm">{name}</p>
+      <p className="text-sm font-medium">{currency}{price}</p>
 
-export default ProductItem
+    </Link>
+  )
+};
+
+export default ProductItem;
